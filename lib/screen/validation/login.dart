f46a5f2e2f0dc9649ee08ccb61b7dc5b064f3a05
Lucasdeekay <90447636+Lucasdeekay@users.noreply.github.com>
2022-10-31 13:24:40 +0100

    Updated login

--- lib/screen/validation/login.dart
+++ lib/screen/validation/login.dart
@@ -33,48 +33,50 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(60.0),
-          child: Card(
-            child: Column(
-              children: [
-                Image.asset(
-                    "assets/images/hannah-rodrigo-mf_3yZnC6ug-unsplash.jpg"),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(4.0, 16.0, 4.0, 4.0),
-                  child: TextField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      label: Text("Full Name"),
-                      hintText: "Enter Your Name",
-                      border: OutlineInputBorder(),
-                      icon: Icon(Icons.person),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: [
+                  Image.asset(
+                      "assets/images/hannah-rodrigo-mf_3yZnC6ug-unsplash.jpg"),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(4.0, 16.0, 4.0, 4.0),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        label: Text("Full Name"),
+                        hintText: "Enter Your Name",
+                        border: OutlineInputBorder(),
+                        icon: Icon(Icons.person),
+                      ),
+                      keyboardType: TextInputType.text,
+                      keyboardAppearance: Brightness.dark,
                     ),
-                    keyboardType: TextInputType.text,
-                    keyboardAppearance: Brightness.dark,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: TextField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      label: Text("Email Address"),
-                      hintText: "Enter Your Email Address",
-                      border: OutlineInputBorder(),
-                      icon: Icon(Icons.mail),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        label: Text("Email Address"),
+                        hintText: "Enter Your Email Address",
+                        border: OutlineInputBorder(),
+                        icon: Icon(Icons.mail),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      keyboardAppearance: Brightness.dark,
+                      onTap: () => {},
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    keyboardAppearance: Brightness.dark,
-                    onTap: () => {},
                   ),
-                ),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: _displayDetails,
-                    child: const Text("Login"),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: _displayDetails,
+                      child: const Text("Login"),
+                    ),
                   ),
-                ),
-                Text(msg),
-              ],
+                  Text(msg),
+                ],
+              ),
             ),
           ),
         ),
